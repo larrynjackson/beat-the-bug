40cba1efa5ad5eed0bd4f7f82a95ba52481a9c94
Larry Jackson <lnjackson@gmail.com>
2023-02-15 10:49:41 -0500

    changed keyboard layout

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,33 +1,8 @@
 import styles from './Keyboard.module.css';
 
-const KEYS = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
+const TOP_KEY_ROW = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
+const MID_KEY_ROW = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+const BOT_KEY_ROW = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
 type KeyboardProps = {
   activeLetters: string[];
@@ -43,28 +18,78 @@ export default function Keyboard({
   addGuessedLetter,
 }: KeyboardProps) {
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
-        gap: '.5rem',
-      }}
-    >
-      {KEYS.map((key) => {
-        const isActive = activeLetters.includes(key);
-        const isInactive = inactiveLetters.includes(key);
-        return (
-          <button
-            onClick={() => addGuessedLetter(key)}
-            className={`${styles.btn} ${isActive ? styles.active : ''}
+    <>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(60px, 1fr))',
+          gap: '.4rem',
+        }}
+      >
+        {TOP_KEY_ROW.map((key) => {
+          const isActive = activeLetters.includes(key);
+          const isInactive = inactiveLetters.includes(key);
+          return (
+            <button
+              onClick={() => addGuessedLetter(key)}
+              className={`${styles.btn} ${isActive ? styles.active : ''}
           ${isInactive ? styles.inactive : ''}`}
-            disabled={isInactive || isActive || disabled}
-            key={key}
-          >
-            {key}
-          </button>
-        );
-      })}
-    </div>
+              disabled={isInactive || isActive || disabled}
+              key={key}
+            >
+              {key}
+            </button>
+          );
+        })}
+      </div>
+      <br />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, min(75px))',
+          gap: '.5rem',
+        }}
+      >
+        {MID_KEY_ROW.map((key) => {
+          const isActive = activeLetters.includes(key);
+          const isInactive = inactiveLetters.includes(key);
+          return (
+            <button
+              onClick={() => addGuessedLetter(key)}
+              className={`${styles.btn} ${isActive ? styles.active : ''}
+          ${isInactive ? styles.inactive : ''}`}
+              disabled={isInactive || isActive || disabled}
+              key={key}
+            >
+              {key}
+            </button>
+          );
+        })}
+      </div>
+      <br />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, min(80px)',
+          gap: '.5rem',
+        }}
+      >
+        {BOT_KEY_ROW.map((key) => {
+          const isActive = activeLetters.includes(key);
+          const isInactive = inactiveLetters.includes(key);
+          return (
+            <button
+              onClick={() => addGuessedLetter(key)}
+              className={`${styles.btn} ${isActive ? styles.active : ''}
+          ${isInactive ? styles.inactive : ''}`}
+              disabled={isInactive || isActive || disabled}
+              key={key}
+            >
+              {key}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 }
